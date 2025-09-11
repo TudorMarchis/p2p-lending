a633750302a66d21aa3ffbc1c0a632359f996001
Tudor Marchis <tudormarchis2009@gmail.com>
2025-09-11 12:43:09 +0300

    stripe, prism, react setup

---
+++ p2p-loaning-app/pages/index.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Credio</h1>
+      <h2>P-2-P money landing platform</h2>
+    </div>
+  );
+}
